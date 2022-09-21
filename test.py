82a07932f5ef78acd9f9417a352fa8b0f087106a
Aayush Saklani <saklania250@gmail.com>
2022-09-21 21:17:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 loop=0
 while(loop<100):
   loop-=1
-  file=open("LOGS/filename"+str(loop)+".txt",w)
+  file=open("LOGS\filename"+str(loop)+".txt",w)
   file.write("\n Duplicate file")
   file.close()
